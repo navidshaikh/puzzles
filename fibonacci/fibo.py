2f78abd13a383b487f2638ebb2fbe6533deb32c1
Navid Shaikh <nshaikh@redhat.com>
2018-02-04 13:33:40 +0530

    Adds fibonacci series calculation and printing program

---
+++ fibonacci/fibo.py
@@ -0,0 +1,42 @@
+def fibo1(n):
+    if n <= 1:
+        return 1
+    return fibo(n-1) + fibo(n - 2)
+
+def fibo2(n):
+    if n < 0:
+        raise ValueError("Input value n can't be less than 0.")
+    if n in [0, 1]:
+        return 1
+
+    prev = 1
+    prev_prev = 0
+    for _ in range(n):
+        current = prev + prev_prev
+        prev_prev = prev
+        prev = current
+    return current
+
+def fibo2_sequence(n):
+    if n < 0:
+        raise ValueError("Input value n can't be less than 0.")
+    if n in [0, 1]:
+        return 1
+
+    prev_prev = 0
+    prev = 1
+    print pre,
+    for _ in range(n):
+        current = prev + prev_prev
+        prev_prev = prev
+        prev = current
+        print current,
+
+    return current
+
+
+if __name__ == "__main__":
+    # print fibo1(8)
+    # print fibo2(8)
+    fibo2_sequence(8)
+
